aaa548a97ccacdf2f1653def0770ea08df9b8074
Kiryl Ramanouski <Kiryl_Ramanouski@epam.com>
2023-03-23 15:16:41 +0200

    feat: try js es6 features in node

--- global.js
+++ global.js
@@ -1,1 +1,6 @@
-console.log("Node is here");
+let hello = "Node is here";
+let justHere = hello.slice(8);
+
+console.log(hello);
+console.log(justHere);
+console.log(`What is ${justHere}? ${hello}!`);
