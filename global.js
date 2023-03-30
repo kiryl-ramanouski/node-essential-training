5c9a106d5cce301473dfe8f3b7213986bb212afe
Kiryl Ramanouski <Kiryl_Ramanouski@epam.com>
2023-03-30 17:01:00 +0300

    faet: use setInterval and clearInterval in node

--- global.js
+++ global.js
@@ -1,6 +1,20 @@
 const waitTime = 3000;
 
-console.log(`Setting a ${waitTime / 1000} seconds delay`);
-const timerFinished = () => console.log("Done");
+console.log(`Setting a ${waitTime / 1000} seconds delay \n`);
+const timerFinished = () => {
+    clearInterval(interval);
+    console.log("Done");
+};
 
 setTimeout(timerFinished, waitTime);
+
+const waitInterval = 500;
+
+let currentTime = 0;
+
+const incTime = () => {
+    currentTime += waitInterval;
+    console.log(`Waiting ${currentTime / 1000} seconds`);
+};
+
+const interval = setInterval(incTime, waitInterval);
