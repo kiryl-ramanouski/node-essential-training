8b3d32357faebbd418067569a253c49dfc773cae
Kiryl Ramanouski <Kiryl_Ramanouski@epam.com>
2023-03-30 16:14:46 +0300

    feat: add listener for answers and questions for node user

--- global.js
+++ global.js
@@ -1,1 +1,20 @@
 process.stdout.write("Hello \n");
+
+const questions = [
+    "What is your name?",
+    "What would you rather be doing?",
+    "What is your preferred programming language?",
+];
+
+const answers = [];
+
+const ask = (i) => {
+    process.stdout.write(`\n ${questions[i]}`);
+    process.stdout.write(` > `);
+}
+
+process.stdin.on("data", (data) => {
+    process.stdout.write(data.toString().trim());
+})
+
+ask(answers.length);
