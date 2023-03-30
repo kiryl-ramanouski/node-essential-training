bb76dd137d750697167c614a7198198e4c93b0c9
Kiryl Ramanouski <Kiryl_Ramanouski@epam.com>
2023-03-30 14:52:45 +0300

    feat: check how to collect user console input

--- global.js
+++ global.js
@@ -1,1 +1,10 @@
-console.log(process.argv);
+const grab = (flag) => {
+    let indexAfterFlag = process.argv.indexOf(flag) + 1;
+    return process.argv[indexAfterFlag];
+}
+
+let greeting = grab("--greeting");
+let user = grab("--user");
+
+console.log(greeting);
+console.log(user);
