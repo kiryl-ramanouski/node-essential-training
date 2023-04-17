cb818839ed18a6c956e587f6890babf1a8fbdc1b
Kiryl Ramanouski <Kiryl_Ramanouski@epam.com>
2023-04-17 13:12:13 +0300

    feat: using READLINE

--- ask.js
+++ ask.js
@@ -5,6 +5,25 @@ const readlineInterface = readline.createInterface({
     output: process.stdout,
 });
 
-readlineInterface.question("How do you like Node?\n", (answer) => {
-    console.log(`Your answer: ${answer}`);
+const questions = ["What is your name?\n", "Where do you live?\n", "What are you going to do with Node.js?\n"];
+
+const collectAnswers = (questions, done) => {
+    const answers = [];
+
+    const questionAnswered = (answer) => {
+        answers.push(answer.trim());
+        if (answers.length < questions.length) {
+            readlineInterface.question(questions[answers.length], questionAnswered);
+        } else {
+            return done(answers);
+        }
+    };
+
+    readlineInterface.question(questions[0], questionAnswered);
+};
+
+collectAnswers(questions, (answers) => {
+    console.log("Thank you for your answers!");
+    console.log(answers);
+    process.exit();
 });
